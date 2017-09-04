25cf7a17d5b435e2aad84c7d1c76bbe61c840656
Woodsmike <Woodsmike@users.noreply.github.com>
2017-09-03 23:11:44 -0400

    Worked through 3rd section in dictionaries

--- Dictionary-Sets.py
+++ Dictionary-Sets.py
@@ -82,27 +82,27 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
 # print(newString)
 # print(newLetters)
 # print(newNumbers)
-
-locations = {0: "You are sitting in front of a computer learning Python",
-             1: "You are standing at the end of the road before a small brick building,",
-             2: "You are on top of a hill",
-             3: "You are inside a building, a well house for a small stream",
-             4: "You are in a valley beside a stream",
-             5: "You are in a forest"}
-
-exits = {0: {"Q": 0},
-         1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
-         2: {"N": 5, "Q": 0},
-         3: {"W": 1, "Q": 0},
-         4: {"N": 1, "W": 2, "Q": 0},
-         5: {"W": 2, "S": 1, "Q": 0}}
-
-vocabulary = {"QUIT": "Q",
-              "NORTH": "N",
-              "SOUTH": "S",
-              "EAST": "E",
-              "WEST": "W"}
-
+#
+# locations = {0: "You are sitting in front of a computer learning Python",
+#              1: "You are standing at the end of the road before a small brick building,",
+#              2: "You are on top of a hill",
+#              3: "You are inside a building, a well house for a small stream",
+#              4: "You are in a valley beside a stream",
+#              5: "You are in a forest"}
+#
+# exits = {0: {"Q": 0},
+#          1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+#          2: {"N": 5, "Q": 0},
+#          3: {"W": 1, "Q": 0},
+#          4: {"N": 1, "W": 2, "Q": 0},
+#          5: {"W": 2, "S": 1, "Q": 0}}
+#
+# vocabulary = {"QUIT": "Q",
+#               "NORTH": "N",
+#               "SOUTH": "S",
+#               "EAST": "E",
+#               "WEST": "W"}
+#
 
 # loc = 1
 # while True:
@@ -129,6 +129,60 @@ vocabulary = {"QUIT": "Q",
 # print(locations[3].split(","))
 # print(" ".join(locations[0].split()))
 
+# loc = 1
+# while True:
+#     availableExits = ", ".join(exits[loc].keys())
+#
+#     print(locations[loc])
+#
+#     if loc == 0:
+#         break
+#
+#     direction = input("Available exits are " + availableExits + " ").upper()
+#     print()
+#     if len(direction) > 1:
+#         words = direction.split()
+#         for word in words:
+#             if word in vocabulary:
+#                 direction = vocabulary[word]
+#     if direction in exits[loc]:
+#         loc = exits[loc][direction]
+#     else:
+#         print("You cannot go in that direction")
+
+# ******
+locations = {0: "You are sitting in front of a computer learning Python",
+             1: "You are standing at the end of the road before a small brick building,",
+             2: "You are on top of a hill",
+             3: "You are inside a building, a well house for a small stream",
+             4: "You are in a valley beside a stream",
+             5: "You are in a forest"}
+
+namedExits = {1: {"2": 2, "3": 3, "5": 5, "4": 4},
+              2: {"5": 5},
+              3: {"1": 1},
+              4: {"1": 1, "2": 2},
+              5: {"2": 2, "1": 1}}
+
+exits = {0: {"Q": 0},
+         1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         2: {"N": 5, "Q": 0},
+         3: {"W": 1, "Q": 0},
+         4: {"N": 1, "W": 2, "Q": 0},
+         5: {"W": 2, "S": 1, "Q": 0}}
+
+vocabulary = {"QUIT": "Q",
+              "NORTH": "N",
+              "SOUTH": "S",
+              "EAST": "E",
+              "WEST": "W",
+              "ROAD": "1",
+              "HILL": "2",
+              "BUILDING": "3",
+              "VALLEY": "4",
+              "FOREST": "5"}
+
+
 loc = 1
 while True:
     availableExits = ", ".join(exits[loc].keys())
@@ -137,15 +191,17 @@ while True:
 
     if loc == 0:
         break
+    else:
+        allExits = exits[loc].copy()
+        allExits.update(namedExits[loc])
 
     direction = input("Available exits are " + availableExits + " ").upper()
     print()
     if len(direction) > 1:
-        words = direction.split()
-        for word in words:
-            if word in vocabulary:
+        for word in vocabulary:
+            if word in direction:
                 direction = vocabulary[word]
-    if direction in exits[loc]:
-        loc = exits[loc][direction]
+    if direction in allExits:
+        loc = allExits[direction]
     else:
         print("You cannot go in that direction")
