d0e643a482ef6df62c755dacb95cfbc3f152c691
Woodsmike <Woodsmike@users.noreply.github.com>
2017-09-04 00:21:23 -0400

    Finished Sets and Dictionaries

--- Dictionary-Sets.py
+++ Dictionary-Sets.py
@@ -151,57 +151,153 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
 #         print("You cannot go in that direction")
 
 # ******
-locations = {0: "You are sitting in front of a computer learning Python",
-             1: "You are standing at the end of the road before a small brick building,",
-             2: "You are on top of a hill",
-             3: "You are inside a building, a well house for a small stream",
-             4: "You are in a valley beside a stream",
-             5: "You are in a forest"}
+# Added another dictionary and added a previous dictionary to it.
 
-namedExits = {1: {"2": 2, "3": 3, "5": 5, "4": 4},
-              2: {"5": 5},
-              3: {"1": 1},
-              4: {"1": 1, "2": 2},
-              5: {"2": 2, "1": 1}}
+# locations = {0: "You are sitting in front of a computer learning Python",
+#              1: "You are standing at the end of the road before a small brick building,",
+#              2: "You are on top of a hill",
+#              3: "You are inside a building, a well house for a small stream",
+#              4: "You are in a valley beside a stream",
+#              5: "You are in a forest"}
+#
+# namedExits = {1: {"2": 2, "3": 3, "5": 5, "4": 4},
+#               2: {"5": 5},
+#               3: {"1": 1},
+#               4: {"1": 1, "2": 2},
+#               5: {"2": 2, "1": 1}}
+#
+# exits = {0: {"Q": 0},
+#          1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+#          2: {"N": 5, "Q": 0},
+#          3: {"W": 1, "Q": 0},
+#          4: {"N": 1, "W": 2, "Q": 0},
+#          5: {"W": 2, "S": 1, "Q": 0}}
+#
+# vocabulary = {"QUIT": "Q",
+#               "NORTH": "N",
+#               "SOUTH": "S",
+#               "EAST": "E",
+#               "WEST": "W",
+#               "ROAD": "1",
+#               "HILL": "2",
+#               "BUILDING": "3",
+#               "VALLEY": "4",
+#               "FOREST": "5"}
+#
+#
+# loc = 1
+# while True:
+#     availableExits = ", ".join(exits[loc].keys())
+#
+#     print(locations[loc])
+#
+#     if loc == 0:
+#         break
+#     else:
+#         allExits = exits[loc].copy()
+#         allExits.update(namedExits[loc])
+#
+#     direction = input("Available exits are " + availableExits + " ").upper()
+#     print()
+#     if len(direction) > 1:
+#         for word in vocabulary:
+#             if word in direction:
+#                 direction = vocabulary[word]
+#     if direction in allExits:
+#         loc = allExits[direction]
+#     else:
+#         print("You cannot go in that direction")
+# --------------------------
 
-exits = {0: {"Q": 0},
-         1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
-         2: {"N": 5, "Q": 0},
-         3: {"W": 1, "Q": 0},
-         4: {"N": 1, "W": 2, "Q": 0},
-         5: {"W": 2, "S": 1, "Q": 0}}
+# -----------SETS-------------
+#
+# farmAnimals = {"sheep", "cow", "hen"}
+# print(farmAnimals)
+#
+# for animal in farmAnimals:
+#     print(animal)
+#
+# print("=" * 40)
+#
+# wildAnimals = set(["lion", "tiger", "panther", "elephant", "hare"])
+# print(wildAnimals)
+#
+# for animal in wildAnimals:
+#     print(animal)
+#
+# farmAnimals.add("horse")
+# wildAnimals.add("horse")
+# print(farmAnimals)
+# print(wildAnimals)
+#
+# emptySet = set()
+# emptySet.add("a")
+#
+# even = set(range(0, 42, 2))
+# print(even)
+# squaresTuple = (4, 6, 9, 16,)
+# squares = set(squaresTuple)
+# print(squares)
+#
+# even = set(range(0, 40, 2))
+# print(even)
+# print(len(even))
+#
+# squaresTuple = (4, 6, 9, 16, 25)
+# squares = set(squaresTuple)
+# print(squares)
+# print(len(squares))
+#
+# print(even.union(squares))
+# print(len(even.union(squares)))
+#
+# print('=' * 40)
+#
+# print(even.intersection(squares))
+# print(even & squares)
+# print(squares.intersection(even))
+# print(squares & even)
+#
+# # subtraction
+#
+# print(sorted(even))
+# print(sorted(squares))
+#
+# print("even minus squares")
+# print(sorted(even.difference(squares)))
+# print(sorted(even - squares))
 
-vocabulary = {"QUIT": "Q",
-              "NORTH": "N",
-              "SOUTH": "S",
-              "EAST": "E",
-              "WEST": "W",
-              "ROAD": "1",
-              "HILL": "2",
-              "BUILDING": "3",
-              "VALLEY": "4",
-              "FOREST": "5"}
+# remove/discard]
 
+# even = set(range(0, 42, 2))
+# print(even)
+# squaresTuple = (4, 6, 9, 16,)
+# squares = set(squaresTuple)
+# print(squares)
+#
+# squares.discard(4)
+# squares.remove(16)
+# print(squares)
+#
+# squares.add(4)
+# squares.add(16)
+# squares.remove(9)
+#
+# if squares.issubset(even):
+#     print("is subset")
+#
+# if even.issuperset(squares):
+#     print("is superset")
 
-loc = 1
-while True:
-    availableExits = ", ".join(exits[loc].keys())
+# ----------Challenge--------------
+# Create a program that takes some text and returns a list of all the characters
+# in the text that are not vowels, sorted in alphabetical order
 
-    print(locations[loc])
+# You can either enter the text from the keyboard or initialize a string variable with the string.
 
-    if loc == 0:
-        break
-    else:
-        allExits = exits[loc].copy()
-        allExits.update(namedExits[loc])
+testText = (input("enter a sentence: ", ))
 
-    direction = input("Available exits are " + availableExits + " ").upper()
-    print()
-    if len(direction) > 1:
-        for word in vocabulary:
-            if word in direction:
-                direction = vocabulary[word]
-    if direction in allExits:
-        loc = allExits[direction]
-    else:
-        print("You cannot go in that direction")
+vowels = frozenset("aeiou")
+finalSet = set(testText).difference(vowels)
+finalSet.discard(" ")
+print(sorted(finalSet))
